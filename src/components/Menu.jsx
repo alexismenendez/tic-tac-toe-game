c13ee971ab7633385ca0cc53a11c5f8b2a6b0edb
Lexi Menendez <amenendez146@gmail.com>
2024-05-16 13:47:57 -0500

    adjusting main menu styling. Adding X and Y buttons to the main menu. Changing from standard html to be compatible with react

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,35 +3,29 @@ import "../styles/style.css"
 
 const Menu = () => {
     return(
-        <div>
-        <h2>MENU!</h2>
-        {/* // <div>
-        //     <div class="logo">
-        //         <h1 id="xLogo">X</h1>
-        //         <h1 id="oLogo">O</h1>
-        //     </div>
+        <div className="Menu">
+            <div className="logo">
+                <h1 id="xLogo">X</h1>
+                <h1 id="oLogo">O</h1>
+            </div>
 
-        //     <div class="playerSelect">
-        //         <div class="selectContainer">
+            <div className="playerSelect">
+                <div className="selectContainer">
 
-        //             <p id="pickMarkText">PICK PLAYER 1'S MARK</p>
+                    <p id="pickMarkText">PICK PLAYER 1&apos;S MARK</p>
 
-        //             <div class="optionContainer">
-        //                 <input type="radio" name="select" id="option-x" class="option" style="display:none;" checked>
-        //                 <label for="option-x" class="option option-x">
-        //                     <span>X</span>
-        //                 </label>
-                        
-        //                 <input type="radio" name="select" id="option-o" class="option" style="display:none;">
-        //                 <label for="option-o" class="option option-o">
-        //                     <span>O</span>
-        //                 </label>
-        //             </div>
-        //         </div>
-        //         <btn type="submit" id="vsComputer" class="landingButton">NEW GAME (VS CPU)</btn>
-        //         <btn type="submit" id="vsPlayer" class="landingButton">NEW GAME (VS PLAYER)</btn>
-        //     </div>
-        // </div> */}
+
+                    <div className="optionContainer">
+                        <input className="option" type="radio" name="player" value="playerX" />
+                        <label htmlFor="playerX">X</label>
+                        <input className="option" type="radio" name="player" value="playerY" />
+                        <label htmlFor="playerY">Y</label>
+                    </div>
+
+                </div>
+                {/* <btn type="submit" id="vsComputer" class="landingButton">NEW GAME (VS CPU)</btn>
+                <btn type="submit" id="vsPlayer" class="landingButton">NEW GAME (VS PLAYER)</btn> */}
+            </div>
         </div>
     )
 }
